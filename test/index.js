3eb6ffade93f9a483fa298b0a8427ed842a316cc
Jeffrey Gilbert <visualjeff@icloud.com>
2017-10-18 15:34:26 -0700

    added a few tests

--- test/index.js
+++ test/index.js
@@ -17,8 +17,7 @@ const expect = Code.expect;
 
 describe('Hapi logging to syslog', () => {
 
-    it('schema validation passes', (done) => {
-
+    it('Default configurations passed schema validation', (done) => {
         const server = new Hapi.Server();
 
         //Configure http
@@ -28,18 +27,15 @@ describe('Hapi logging to syslog', () => {
         });
 
         server.register({
-
             register: Plugin,
             options: {}
         }, (err) => {
-
             expect(err).to.be.undefined();
         });
         done();
     });
-/*
-    it('schema validation passes', (done) => {
 
+    it('Custom parameters pass schema validation', (done) => {
         const server = new Hapi.Server();
 
         //Configure http
@@ -49,19 +45,26 @@ describe('Hapi logging to syslog', () => {
         });
 
         server.register({
-            register: require('../'),
+            register: Plugin,
             options: {
-                proxy: false
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Tcp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
             }
         }, (err) => {
-
             expect(err).to.be.undefined();
         });
         done();
     });
 
-    it('schema validation fails because of extra option value', (done) => {
-
+    it('Custom parameters fail schema validation', (done) => {
         const server = new Hapi.Server();
 
         //Configure http
@@ -71,18 +74,574 @@ describe('Hapi logging to syslog', () => {
         });
 
         server.register({
-            register: require('../'),
+            register: Plugin,
             options: {
-                proxy: false,
+                target: 99,
+		syslogHostname: 'localhost',
+		transport: 'Tcp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"target" must be a string');
+        });
+        done();
+    });
+
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'not a valid hostname',
+		syslogHostname: 'localhost',
+		transport: 'Tcp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"target" must be a valid hostname');
+        });
+        done();
+    });
+
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 98,
+		transport: 'Tcp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"syslogHostname" must be a string');
+        });
+        done();
+    });
+
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'not a valid hostname',
+		transport: 'Tcp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"syslogHostname" must be a valid hostname');
+        });
+        done();
+    });
+    
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 9,
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"transport" must be a string');
+        });
+        done();
+    });
+    
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 'abc',
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"port" must be a number');
+        });
+        done();
+    });
+    
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 1.2,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"port" must be an integer');
+        });
+        done();
+    });
+
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 'abc',
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"tcpTimeout" must be a number');
+        });
+        done();
+    });
+
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 1.2,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"tcpTimeout" must be an integer');
+        });
+        done();
+    });
+
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: 'astring',
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"rfc3164" must be a boolean');
+        });
+        done();
+    });
+
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: 123,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"rfc3164" must be a boolean');
+        });
+        done();
+    });
+    
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 123,
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"appName" must be a string');
+        });
+        done();
+    });
+
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: true,
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"appName" must be a string');
+        });
+        done();
+    });
+    
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: '(d) => date.toISOString()',
+		facility: 23,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"dateFormatter" must be a Function');
+        });
+        done();
+    });
+    
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 'info',
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"facility" must be a number');
+        });
+        done();
+    });
+    
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 1.2,
+                severity: 6
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"facility" must be an integer');
+        });
+        done();
+    });
+    
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 'error'
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"severity" must be a number');
+        });
+        done();
+    });
+    
+    it('Custom parameters fail schema validation', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
+                target: 'localhost',
+		syslogHostname: 'localhost',
+		transport: 'Udp',
+		port: 514,
+		tcpTimeout: 5000,
+		rfc3164: true,
+		appName: 'test',
+		dateFormatter: (d) => date.toISOString(),
+		facility: 23,
+                severity: 6.2
+            }
+        }, (err) => {
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.name).to.equal('ValidationError');
+            expect(err.details[0].message).to.equal('"severity" must be an integer');
+        });
+        done();
+    });
+    
+    it('schema validation fails because of extra unexpected option value', (done) => {
+        const server = new Hapi.Server();
+
+        //Configure http
+        server.connection({
+            host: '0.0.0.0',
+            port: 3000
+        });
+
+        server.register({
+            register: Plugin,
+            options: {
                 dumb: 'foo'
             }
         }, (err) => {
-
             expect(err).to.be.an.instanceof(Error);
             expect(err.name).to.equal('ValidationError');
             expect(err.details[0].message).to.equal('"dumb" is not allowed');
         });
         done();
     });
-*/
+
 });
